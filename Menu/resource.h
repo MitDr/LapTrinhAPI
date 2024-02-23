ecbf888de80dd097fc80290597c121f883891cf1
MitDr <ttlong1301@gmail.com>
2024-02-23 20:14:51 +0700

    menuHomeWork

---
+++ Menu/resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Menu.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MENU_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDI_MENU                        107
+#define IDI_SMALL                       108
+#define IDC_MENU                        109
+#define IDR_MAINFRAME                   128
+#define IDR_MAIN                        130
+#define IDR_MENU                        131
+#define ID_FILE_NEW32776                32776
+#define ID_FILE_OPEN32777               32777
+#define ID_FILE_CLOSE32778              32778
+#define ID_LANGUAGES_ENGLISH32779       32779
+#define ID_LANGUAGES_VIETNAMESE32780    32780
+#define ID_Menu                         32781
+#define ID_FILE_CLOSE32782              32782
+#define ID_T32785                       32785
+#define ID_T32786                       32786
+#define ID_T32787                       32787
+#define ID_T32788                       32788
+#define ID_NG32789                      32789
+#define ID_NG32790                      32790
+#define ID_V                            32791
+#define ID_E                            32792
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32793
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
